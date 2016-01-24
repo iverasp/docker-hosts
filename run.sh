13c0cb3142af7e065ffd8b2942b0d79ad990e067
Iver Egge <iverasp@gmail.com>
2016-01-24 15:17:20 +0100

    oops

--- run.sh
+++ run.sh
@@ -1,10 +1,1 @@
-mkdir -p /var/lib/docker
-touch /var/lib/docker/hosts
-chmod 644 /var/lib/docker/hosts
-chown nobody:nobody /var/lib/docker/hosts
-
-docker run \
-    -d \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /var/lib/docker/hosts:/srv/hosts \
-    iverasp/hosts --domain-name=dev.docker /srv/hosts
+./stage/docker-host /srv/var/hosts
